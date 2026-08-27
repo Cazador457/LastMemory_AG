68c6ceded13bd1008e4e604249425b5c7396f669
Angel Bernardino Melquiades <chiv_157@hotmail.com>
2026-08-27 14:54:02 -0600

    Notes

--- LastMemory/Assets/Script/Combat.cs
+++ LastMemory/Assets/Script/Combat.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Combat : MonoBehaviour
 {
+    //
+    private RavenInputActions inputActions;
+
+    //
     public float damage = 5;
     public float speed = 5;
     public float defence = 9;
     public int nockBack = 2;
-    
+
+    private void Awake()
+    {
+        inputActions = new RavenInputActions();
+    }
     void Start()
     {
         
@@ -16,4 +25,16 @@ public class Combat : MonoBehaviour
     {
         
     }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        //inputActions.Player.Jump.performed += OnJump;
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+        //inputActions.Player.Jump.performed -= OnJump;
+    }
+    //public void Jump()
+
 }
