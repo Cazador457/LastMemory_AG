62e6a442e31cc889d8f1d1f65b0332db09088832
Angel Bernardino Melquiades <Raven.BFlame@gmail.com>
2026-09-17 11:56:01 -0600

    PlayerInputAdvance

--- LastMemory/Assets/Script/PlayerController.cs
+++ LastMemory/Assets/Script/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,15 +7,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private RavenInputActions RavenInput;
 
     [SerializeField] private CharacterController playerController;
-    private Animator Animation;
+    [SerializeField] private Transform Camera;
     public float speed = 5f;
+    public float sprint = 7f;
     public float sensity = 200;
-    public Transform Player;
-    public float XRotation=0f;
+    private float XRotation = 0f;
+
+    private Vector3 verticalVelocity;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float fallGravityMultipler = 2f;
+    private bool IsGrounded;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float jumpCutMultipler = 2f;
 
     void Start()
     {
-        Animation=GetComponent<Animator>();
+        playerController=GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -26,61 +34,66 @@ public class PlayerController : MonoBehaviour
     }
     private void Movement()
     {
+        IsGrounded = playerController.isGrounded;
+        if (IsGrounded && verticalVelocity.y < 0) 
+            verticalVelocity.y = -2f;
+
         Vector2 movement = RavenInput.move;
         Vector3 FinalDirectoion = (transform.forward * movement.y) + (transform.right * movement.x);
-        transform.position += FinalDirectoion * speed * Time.deltaTime;
+        playerController.Move(FinalDirectoion * speed * Time.deltaTime);
+
+        float currentVelocity = gravity;
+        if (verticalVelocity.y < 0) currentVelocity *= fallGravityMultipler;
+
+        verticalVelocity.y += currentVelocity * Time.deltaTime;
+        playerController.Move(verticalVelocity*Time.deltaTime);
     }
     private void Loock()
     {
         Vector2 look = RavenInput.look;
         float MouseX = look.x * sensity * Time.deltaTime;
         float MouseY = look.y * sensity * Time.deltaTime;
-
+        //Body Rotate
+        transform.Rotate(Vector3.up * MouseX);
+        //Camera Rota
         XRotation += MouseY;
         XRotation = Mathf.Clamp(XRotation, -75f, 75f);
-        transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
-
-        Player.Rotate(Vector3.up * MouseX);
+        Camera.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
     }
     private void ActionsGame()
     {
-        if (RavenInput.SprintHeld)
-        {
-            Sprint();
-        }
+        if (RavenInput.SprintHeld) Sprint();
+        if (RavenInput.SprintReleased) speed = 5f;
+
         // DEFENDER
         if (RavenInput.DefendPressed)
-        {
             Defend(true);
-        }
-
         if (RavenInput.DefendReleased)
-        {
             Defend(false);
-        }
+        // JUMP CUT
+        if (RavenInput.JumpReleased && verticalVelocity.y > 0)
+            verticalVelocity.y /= jumpCutMultipler;
     }
     private void Jump()
     {
-        
-        Debug.Log("saltando");
+        if (playerController.isGrounded)
+            verticalVelocity.y += Mathf.Sqrt(jumpForce * -2f * gravity);
     }
     private void Attack()
     {
-        Animation.SetTrigger("Attack");
         Debug.Log("Atacando");
     }
     private void Sprint()
     {
-        
-        Debug.Log("Sprint");
+        speed = sprint;
     }
+
     private void Interact()
     {
         Debug.Log("Interactuando");
     }
     private void Defend(bool defending)
     {
-        Animation.SetBool("Defend", defending);
         Debug.Log("Defender+" + defending);
     }
     private void OnEnable()
